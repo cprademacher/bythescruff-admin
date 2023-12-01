5ed0e0e773cff4faa6a359162c7ba1a99e64897a
Cody Rademacher <66492356+cprademacher@users.noreply.github.com>
2023-12-01 16:45:36 -0600

    Starting home page

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,16 @@
 import Layout from "@/components/Layout"
+import {useSession} from "next-auth/react";
+// import Image from "next/image"
 
 export default function Home() {
+  const {data: session} = useSession();
+
   return <Layout>
-    Home Page
+    <div className="text-blue-900">
+      Hello, {session?.user?.name} 
+      <img src={session?.user?.image} alt="User profile photo" />
+    </div>
   </Layout>
 }
+
+// Check image by console.log(session.user.image) and making sure I have one before continuing.
