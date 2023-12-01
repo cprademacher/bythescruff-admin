04b1b6bd19ee5a43239f7ecdb3941baab76e4f22
Cody Rademacher <66492356+cprademacher@users.noreply.github.com>
2023-12-01 09:39:04 -0600

    Set up mongodb and started nav

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,5 +1,7 @@
+import { MongoDBAdapter } from "@auth/mongodb-adapter";
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import clientPromise from "@/lib/mongodb";
 
 export default NextAuth({
   providers: [
@@ -9,4 +11,5 @@ export default NextAuth({
       clientSecret: process.env.GOOGLE_SECRET,
     }),
   ],
+  adapter: MongoDBAdapter(clientPromise),
 });
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import { useSession, signIn, signOut } from "next-auth/react";
+import Nav from "@/components/nav";
 
 export default function Home() {
   const { data: session } = useSession();
 
   if (!session) {
+    // What the user see's if not logged in
     return (
       <div className="bg-blue-900 w-screen h-screen flex items-center">
         <div className="text-center w-full">
@@ -18,9 +20,11 @@ export default function Home() {
     );
   }
 
+  // What the user see's if they are logged in
   return (
-    <div className="bg-blue-900 w-screen h-screen flex items-center">
-      <div className="text-center w-full">
+    <div className="bg-blue-900 w-screen h-screen">
+      <Nav />
+      <div>
         <h1>Logged In {session.user.email}</h1>
         <button
           onClick={() => signOut()}
