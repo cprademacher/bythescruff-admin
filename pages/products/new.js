d10965919a97a213b02fd300945a05b3f388a7af
Cody Rademacher <66492356+cprademacher@users.noreply.github.com>
2023-12-04 14:16:26 -0600

    Adding functionality to add a product to mongodb database

--- pages/products/new.js
+++ pages/products/new.js
@@ -1,10 +1,47 @@
 import Layout from "@/components/Layout";
+import { useState } from "react";
+import axios from "axios";
 
 export default function NewProduct() {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [price, setPrice] = useState("");
+
+  async function createProduct(event) {
+    event.preventDefault();
+    const data = { title, description, price };
+    await axios.post("/api/products", data);
+  }
+
   return (
     <Layout>
-    <input type="text" placeholder="Product Name" />
-    <textarea type="text" placeholder="Description" />
-  </Layout>
+      <form onSubmit={createProduct}>
+        <h1>New Product</h1>
+        <label>Product Name</label>
+        <input
+          type="text"
+          placeholder="Product Name"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <label>Description</label>
+        <textarea
+          type="text"
+          placeholder="Description"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+        />
+        <label>Price (in USD)</label>
+        <input
+          type="number"
+          placeholder="Price"
+          value={price}
+          onChange={(e) => setPrice(e.target.value)}
+        />
+        <button type="submit" className="btn-primary">
+          Save
+        </button>
+      </form>
+    </Layout>
   );
 }
