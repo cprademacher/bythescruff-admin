0b4a14921da9c42bee96dd102fa45bba74eec6f6
Cody Rademacher <66492356+cprademacher@users.noreply.github.com>
2023-12-08 10:32:48 -0600

    About to add images to return statement

--- components/ProductFrom.js
+++ components/ProductFrom.js
@@ -7,11 +7,12 @@ export default function ProductForm({
   title: existingTitle,
   description: existingDescription,
   price: existingPrice,
-  images,
+  images: existingImages,
 }) {
   const [title, setTitle] = useState(existingTitle || "");
   const [description, setDescription] = useState(existingDescription || "");
   const [price, setPrice] = useState(existingPrice || "");
+  const [images, setImages] = useState(existingImages || []);
   const [goToProducts, setGoToProducts] = useState(false);
   const router = useRouter();
   console.log({ _id });
@@ -40,11 +41,10 @@ export default function ProductForm({
       for (const file of files) {
         data.append("file", file);
       }
-      const res = await fetch("/api/upload", {
-        method: "POST",
-        body: data,
+      const res = await axios.post("/api/upload", data);
+      setImages((oldImages) => {
+        return [...oldImages, ...res.data.links];
       });
-      console.log(res);
     }
   }
 
@@ -59,6 +59,7 @@ export default function ProductForm({
       />
       <label>Photos</label>
       <div className="mb-2">
+        
         <label className="w-24 h-24 flex items-center cursor-pointer justify-center text-sm gap-1 text-gray-500 rounded-lg bg-gray-200">
           <svg
             xmlns="http://www.w3.org/2000/svg"
