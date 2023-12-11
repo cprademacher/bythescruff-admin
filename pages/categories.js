343652fe0fc3b13d27643ae0bfa2d212b125b37b
Cody Rademacher <66492356+cprademacher@users.noreply.github.com>
2023-12-11 08:43:07 -0600

    Added parent category selection dropdown

--- pages/categories.js
+++ pages/categories.js
@@ -4,6 +4,7 @@ import axios from "axios";
 
 export default function Categories() {
   const [name, setName] = useState("");
+  const [parentCategory, setParentCategory] = useState("");
   const [categories, setCategories] = useState([]);
   useEffect(() => {
     fetchCategories();
@@ -36,6 +37,19 @@ export default function Categories() {
           onChange={(event) => setName(event.target.value)}
           value={name}
         />
+        <select
+          className="mb-0"
+          value={parentCategory}
+          onChange={(event) => setParentCategory(event.target.value)}
+        >
+          <option value="">No Parent Category</option>
+          {categories.length > 0 &&
+            categories.map((category) => (
+              <option key={category._id} value={category._id}>
+                {category.name}
+              </option>
+            ))}
+        </select>
         <button type="submit" className="btn-primary py-1">
           Save
         </button>
